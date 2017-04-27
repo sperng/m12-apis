f2ee5d6966dd8e258c314ed6817068681bfc36af
sperng <sam.perng@live.com>
2017-04-26 17:12:51 -0700

    somehow got simpsons movie from shaolin soccer

--- exercise-4/exercise.r
+++ exercise-4/exercise.r
@@ -16,13 +16,13 @@ movie <- 'Shaolin Soccer'
 # Make a variable called "query.parameters" that holds a list of the parameters
 # to pass to the API. View the OMDb documentation to see which parameters
 # are available.
-query.parameters <- list(i = 'id', t = 'movie', y = 'year', r = 'json')
+query.parameters <- list(t = 'movie', y = 'year', r = 'json')
  
 
 # Make a variable called request that is a string of a request URL made up of the base URL
 # and the parameters string
-response <- GET(base.url, query.parameters)
-body <- content(response, 'text')
+response <- GET(base.url, query = query.parameters)
+body <- content(response, "text")
 request <- paste0(base.url, query.parameters)
 
 
@@ -32,6 +32,7 @@ movie.data <- fromJSON(body)
 
 
 # Make movie_data into a data frame using as.data.frame
+move.df <- as.data.frame(move.data)
 
 
 # Write a function called Director that accepts a data frame of movie info and returns
